fd12aefb1ae5f3af1b240b049fc12bc8f290c41d
ural <sevomax@gmail.com>
2018-01-05 18:28:19 -0500

    added dog pic

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -44,7 +44,7 @@ class GenusController extends Controller {
     $notes = [
       ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
       ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
-      ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+      ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/Dog-2018.png', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
     ];
     $data = [
       'notes' => $notes
