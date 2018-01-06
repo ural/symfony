a9c2af64a772ffba7807fb4cec23bd586ab13912
ural <sevomax@gmail.com>
2018-01-05 19:24:43 -0500

    added react and babel JSs and dynamicly populated 'notes' section

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -16,15 +16,8 @@ class GenusController extends Controller {
    */
   public function showAction ($genusName) {
 
-    $notes = [
-      'A broad three in four Americans, 75%, say they oppose enforcing federal laws against marijuana in states that have legalized medical or recreational use of the drug',
-      'The latest numbers from the National Institute on Drug Abuse show that 44% of Americans over the age of 12 have used marijuana at least once in their lifetime. A majority, 52%, of people ages 18 to 25 have used it in their lifeline, including 33% in just the last 12 months.',
-      'Legal pot has grown to a $6.6 billion industry, with seven in 10 dollars going for medical marijuana and three in 10 going for recreational marijuana. The overall industry has been projected to quadruple over the next decade, according to New Frontier Data, a research company that analyzes the marijuana industry.'
-    ];
-
     return $this->render('genus/show.html.twig', array(
-      'name' => $genusName,
-      'notes' => $notes
+      'name' => $genusName
     ));
 
 /*    $templating = $this->container->get('templating');
@@ -36,15 +29,16 @@ class GenusController extends Controller {
   }
 
   /**
-   * @Route("/genus/{genusName}/notes")
+   * @Route("/genus/{genusName}/", name="genus_show_notes")
    * @Method("GET")
    */
-  public function getNotesAction($genusName)
+  public function getNotesAction()
   {
     $notes = [
       ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
       ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
       ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/Dog-2018.png', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+      ['id' => 4, 'username' => 'Wass', 'avatarUri' => '/images/Dog-2018.png', 'note' => 'WASS IST DASS!', 'date' => 'Jan. 5, 2018'],
     ];
     $data = [
       'notes' => $notes
