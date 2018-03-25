f3b5425f2067cd16d5b6cbfdada3ab6061e7bd3c
ural <sevomax@gmail.com>
2018-03-25 19:35:57 -0400

    commiting left overs

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -2,55 +2,118 @@
 
 namespace AppBundle\Controller;
 
-
+use AppBundle\Entity\Genus;
+use AppBundle\Entity\GenusNote;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController extends Controller {
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/new")
+     */
+    public function newAction()
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
 
-  /**
-   * @Route("/genus/{genusName}")
-   */
-  public function showAction ($genusName) {
+        $note = new GenusNote();
+        $note->setUsername('AquaWeaver');
+        $note->setUserAvatarFilename('ryan.jpeg');
+        $note->setNote('I counted 8 legs... as they wrapped around me');
+        $note->setCreatedAt(new \DateTime('-1 month'));
+        $note->setGenus($genus);
 
-    $funFact = "Octopuses can change the color of their body in just *three-tenths* of a second!";
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->persist($note);
+        $em->flush();
 
-    $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
-    $key = md5($funFact);
-    if ($cache->contains($key)) {
-      $funFact = $cache->fetch($key);
-    } else {
-      // sleep(1);
-      $funFact = $this->get('markdown.parser')
-        ->transform($funFact);
+        return new Response('<html><body>Genus created!</body></html>');
     }
 
-    return $this->render('genus/show.html.twig', array(
-      'name' => $genusName,
-      'funFact' => $funFact
-    ));
-  }
+    /**
+     * @Route("/genus")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
 
-  /**
-   * @Route("/genus/{genusName}/", name="genus_show_notes")
-   * @Method("GET")
-   */
-  public function getNotesAction()
-  {
-    $notes = [
-      ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
-      ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
-      ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/Dog-2018.png', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
-      ['id' => 4, 'username' => 'Wass', 'avatarUri' => '/images/Dog-2018.png', 'note' => 'WASS IST DASS!', 'date' => 'Jan. 5, 2018'],
-      ['id' => 5, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
-    ];
-    $data = [
-      'notes' => $notes
-    ];
+        $genuses = $em->getRepository('AppBundle:Genus')
+            ->findAllPublishedOrderedByRecentlyActive();
 
-    return new JsonResponse($data);
-  }
+        return $this->render('genus/list.html.twig', [
+            'genuses' => $genuses
+        ]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
+     */
+    public function showAction($genusName)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
+
+        if (!$genus) {
+            throw $this->createNotFoundException('genus not found');
+        }
+
+        // todo - add the caching back later
+        /*
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $key = md5($funFact);
+        if ($cache->contains($key)) {
+            $funFact = $cache->fetch($key);
+        } else {
+            sleep(1); // fake how slow this could be
+            $funFact = $this->get('markdown.parser')
+                ->transform($funFact);
+            $cache->save($key, $funFact);
+        }
+        */
+
+        $this->get('logger')
+            ->info('Showing genus: '.$genusName);
+
+        $recentNotes = $em->getRepository('AppBundle:GenusNote')
+            ->findAllRecentNotesForGenus($genus);
+
+        return $this->render('genus/show.html.twig', array(
+            'genus' => $genus,
+            'recentNoteCount' => count($recentNotes)
+        ));
+    }
+
+    /**
+     * @Route("/genus/{name}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction(Genus $genus)
+    {
+        $notes = [];
+
+        foreach ($genus->getNotes() as $note) {
+            $notes[] = [
+                'id' => $note->getId(),
+                'username' => $note->getUsername(),
+                'avatarUri' => '/images/'.$note->getUserAvatarFilename(),
+                'note' => $note->getNote(),
+                'date' => $note->getCreatedAt()->format('M d, Y')
+            ];
+        }
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
+    }
 }
--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,11 +2,12 @@
 
 namespace AppBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class MainController extends Controller {
-  public function homepageAction() {
-    return $this->render('main/homepage.html.twig');
-  }
+class MainController extends Controller
+{
+    public function homepageAction()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
 }
